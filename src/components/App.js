eb9cdd1f5988e1a993b04ef1b9e2742dbeeb83cf
liamnaule <liamnaule@gmail.com>
2025-04-30 02:58:37 +0300

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+// App.js
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [imageUrl, setImageUrl] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setImageUrl(data.message);
+      });
+  }, []);
+
+  if (!imageUrl) {
+    return <p>Loading...</p>;
+  }
+
+  return <img src={imageUrl} alt="A Random Dog" />;
+}
+
+export default App;
